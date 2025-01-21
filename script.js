858e58fb39b36295f823a2de82553e2068623fb0
c3r4h <67233440+c3r4h@users.noreply.github.com>
2025-01-21 18:35:07 +0700

    feat: deploy

---
+++ script.js
@@ -0,0 +1,96 @@
+function doGenerate() {
+    loader.style.display = 'none';
+    uploadedImage.src = '';
+    const url = getRandomImage();
+    uploadedImage.src = url;
+    generateColorPaletteUrl(url);
+}
+
+function getRandomImage() {
+    const urlArray = [
+        "1560487765-67095b892dd1",
+        "1579205149708-f5b24c5a04e5",
+        "1517924030062-84f2c8f0784e",
+        "1482849297070-f4fae2173efe",
+        "1487376480913-24046456a727",
+        "1481456384069-0effc539ab7e",
+        "1524678714210-9917a6c619c2",
+        "1525697472245-fb8fd8594791",
+        "1484755560615-a4c64e778a6c",
+        "1515442304705-de3793c6cce0",
+        "1432139555190-58524dae6a55",
+    ];
+    const randomIndex = Math.floor(Math.random() * urlArray.length);
+    return "https://images.unsplash.com/photo-" + urlArray[randomIndex] + "?w=800&h=550&auto=format&fit=crop&q=60";
+}
+
+function doUpload() {
+    if (selectedFile) {
+        uploadedImage.style.opacity = '0';
+        colorPalette.style.opacity = '0';
+        loader.style.display = 'block';
+        colorPalette.innerHTML = '';
+
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            uploadedImage.src = e.target.result;
+            uploadedImage.onload = function () {
+                generateColorPalette(uploadedImage);
+            };
+        }
+        reader.readAsDataURL(selectedFile);
+    } else {
+        alert('Please select an image file first.');
+    }
+}
+
+function generateColorPalette(image) {
+    const vibrant = new Vibrant(image);
+    var swatches = vibrant.swatches();
+    for (var swatch in swatches) {
+        if (swatches.hasOwnProperty(swatch) && swatches[swatch]) {
+            var color = swatches[swatch].getHex();
+            generateColorBox(color);
+        }
+    }
+
+    uploadedImage.style.opacity = '1';
+    colorPalette.style.opacity = '1';
+    loader.style.display = 'none';
+}
+
+async function generateColorPaletteUrl(url) {
+    uploadedImage.style.opacity = '0';
+    colorPalette.style.opacity = '0';
+    loader.style.display = 'block';
+    colorPalette.innerHTML = '';
+
+    const img = new Image();
+    img.crossOrigin = 'Anonymous';
+    img.src = url;
+    img.onload = function () {
+        const vibrant = new Vibrant(img);
+        const swatches = vibrant.swatches();
+        for (var swatch in swatches) {
+            if (swatches.hasOwnProperty(swatch) && swatches[swatch]) {
+            var color = swatches[swatch].getHex();
+            generateColorBox(color);
+            }
+        }
+
+        uploadedImage.src = url;
+        uploadedImage.style.opacity = '1';
+        colorPalette.style.opacity = '1';
+        loader.style.display = 'none';
+
+        img.remove();
+    };
+}
+
+function generateColorBox(color) {
+    var colorBox = document.createElement('div');
+    colorBox.className = 'color-box flex-1 flex items-center justify-center text-white text-xs';
+    colorBox.style.backgroundColor = color;
+    colorBox.textContent = color;
+    colorPalette.appendChild(colorBox);
+}
